60d8504bcbdc9c2471ac4812b57ecb310e6be7c7
Michael M <mickelsonmichael@gmail.com>
2020-02-17 09:34:49 -0500

    Add inital services

---
+++ Shopkeeper/Shopkeeper.Services/Interfaces/IItemService.cs
@@ -0,0 +1,12 @@
+﻿using Shopkeeper.Business;
+using System.Linq;
+
+namespace Shopkeeper.Services.Interfaces
+{
+    public interface IItemService
+    {
+        Item Get(string itemId);
+        IQueryable<Item> GetAll();
+        Item GetRandom(params string[] exclude);
+    }
+}
---
+++ Shopkeeper/Shopkeeper.Services/ItemService.cs
@@ -0,0 +1,35 @@
+﻿using Shopkeeper.Business;
+using Shopkeeper.Data.Interfaces;
+using Shopkeeper.Services.Interfaces;
+using System;
+using System.Linq;
+
+namespace Shopkeeper.Services
+{
+    public class ItemService : IItemService
+    {
+        private readonly IItemRepository itemRepository;
+        private Random _random;
+
+        private Random Random => _random ?? (_random = new Random());
+
+        public ItemService(IItemRepository itemRepository)
+        {
+            this.itemRepository = itemRepository;
+        }
+
+        public Item Get(string itemId) => itemRepository.Get(itemId);
+
+        public IQueryable<Item> GetAll() => itemRepository.GetAll();
+
+        public Item GetRandom(params string[] exclude)
+        {
+            var items = GetAll()
+                .Where(x => x.HasComponents && !exclude.Contains(x.Id));
+
+            var index = Random.Next(items.Count());
+
+            return items.ElementAt(index);
+        }
+    }
+}
