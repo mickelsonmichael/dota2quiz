d71b1324236fd59a45a1842ece6692312bdc6fb8
Michael M <mickelsonmichael@gmail.com>
2020-02-17 09:33:28 -0500

    Add initial Business object configuration

---
+++ Shopkeeper/Shopkeeper.Business/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Shopkeeper.Business
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string FileName { get; set; }
+        public IEnumerable<string> ComponentNames { get; set; }
+        public IEnumerable<Item> Components { get; set; }
+        public bool HasComponents => Components?.Any() == true;
+        public bool RequiresRecipe { get; set; }
+    }
+}
---
+++ Shopkeeper/Shopkeeper.Business/QuizItems.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Shopkeeper.Business
+{
+    public class QuizItems
+    {
+        public IEnumerable<Item> Items { get; set; }
+    }
+}
