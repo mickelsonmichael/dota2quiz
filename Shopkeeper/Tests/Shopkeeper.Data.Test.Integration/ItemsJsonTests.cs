f963f05d2156c5de99af3d5ad0ae8b04a21599c8
mickelsonmichael <mickelsonmichael@gmail.com>
2020-04-12 07:03:45 -0600

    Add simple test to ensure all components have matching items

---
+++ Shopkeeper/Tests/Shopkeeper.Data.Test.Integration/ItemsJsonTests.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+using Shopkeeper.Business;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace Shopkeeper.Data.Test.Integration
+{
+    public class ItemsJsonTests
+    {
+        private List<Item> _items { get; }
+        private HashSet<string> _itemIds { get; }
+
+        public ItemsJsonTests()
+        {
+            var file = File.ReadAllText("items.json");
+
+            _items = JsonConvert.DeserializeObject<QuizItems>(file).Items;
+            _itemIds = _items.Select(x => x.Id).ToHashSet();
+        }
+
+        [Fact]
+        public void AllComponents_HaveMatchingItem()
+        {
+            foreach (var item in _items.Where(x => x.ComponentNames.Any()))
+            {
+                Assert.True(item.ComponentNames.All(x => _itemIds.Contains(x)));
+            }
+        }
+    }
+}
