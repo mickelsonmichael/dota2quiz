5ddb96ca50f2e8d5748abaac791935256581b3f7
mickelsonmichael <mickelsonmichael@gmail.com>
2021-01-13 14:59:43 -0700

    Break original app into components and move images to public/

--- src/question/index.js
+++ src/question/index.js
@@ -1,6 +1,15 @@
-const Question = () => (
+const Question = ({ item }) => (
     <div id="stage" class="box">
-        Question
+        <div class="target-item-container">
+            <span class="target-item">
+                <img id="target-item" src={`images/${item.file}`} alt={item.name} />
+            </span>
+        </div>
+
+        <div class="selected-items">
+            {item.components.map(c => (<span className="selected-item" />))}
+            {item.recipe && (<span className="selected-item" />)}
+        </div>
     </div>
 );
 
